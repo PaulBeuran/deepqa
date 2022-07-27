0500a4dfb78c00c3d42be1b2c788ccf055e038b7
Paul Beuran <paul.beuran@outlook.fr>
2022-07-27 20:15:40 +0200

    Added wordcloud and text length plots

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,5 @@
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def unwrap_squad11_data(data):
     """
@@ -70,3 +72,38 @@ def tabularize_squad11_data(data):
                     contexts.append(context)
                     answers.append(answer)
     return list(zip(titles, contexts, queries, answers))
+
+
+def get_corpus_tf_idf_word_frequencies(corpus, **tf_idf_vectorizer_kwargs):
+    """
+    Get the TF-IDF frequency for each words (or terms) in the corpus and return it
+    as a dictionary. The TF-IDF computation is done using sklearn's
+    TfidfVectorizer class, and as such can be controlled with its given
+    parameters (ex: tokenizer, stop words, n-grams etc...)
+    (see TfidfVectorizer documenation) 
+
+    Parameters
+    ----------
+    corpus : list
+        List of text or document corpus
+    **tf_idf_vectorizer_kwargs : 
+        Parameters given to the TfidfVectorizer class
+
+    Returns
+    -------
+    dict
+        A dictionary containing vocabulary terms associated with their respective
+        TF-IDF frequency from the corpus
+    """
+    tf_idf_vectorizer = TfidfVectorizer(**tf_idf_vectorizer_kwargs)
+    tf_idf = tf_idf_vectorizer.fit_transform(corpus)
+    tf_idf_by_terms = np.asarray(tf_idf.sum(axis=0)).flatten()
+    terms_tf_idf = (
+        dict(
+            zip(
+                list(tf_idf_vectorizer.vocabulary_.keys()), 
+                tf_idf_by_terms[list(tf_idf_vectorizer.vocabulary_.values())]
+            )
+        )
+    )
+    return terms_tf_idf
