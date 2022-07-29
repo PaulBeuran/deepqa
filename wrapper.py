2855eead78c0bd6a407c3b66d08d885c4d9f59ff
Paul Beuran <paul.beuran@outlook.fr>
2022-07-30 00:58:17 +0200

    Fix no_grad range

--- wrapper.py
+++ wrapper.py
@@ -29,7 +29,7 @@ class QATrainWrapper(torch.nn.Module):
                 data_iter_tqdm.set_description(f"""[{epoch+1}/{epochs} - Train] Loss: {
                     round(epoch_total_loss/(i+1), 3)}, Overlap F1: {
                         round(epoch_total_f1/(i+1), 3)}""")
-                        
+
             if val_data_iter is not None:
                 epoch_total_f1 = 0
                 epoch_total_loss = 0
@@ -37,11 +37,11 @@ class QATrainWrapper(torch.nn.Module):
                 for i, batch in enumerate(data_iter_tqdm):
                     with torch.no_grad():
                         contexts_token_ids, queries_token_ids, answers_tokens_ids = batch
-                    answers_probs = self.model([contexts_token_ids, queries_token_ids])
-                    answer_preds = answers_probs.argmax(dim=1)
-                    loss_step = loss(answers_probs, answers_tokens_ids)
-                    epoch_total_loss += loss_step.item()
-                    epoch_total_f1 += overlap_f1_score(answer_preds, answers_tokens_ids).mean().item()
-                    data_iter_tqdm.set_description(f"""[{epoch+1}/{epochs} - Validation] Loss: {
-                        round(epoch_total_loss/(i+1), 3)}, Overlap F1: {
-                            round(epoch_total_f1/(i+1), 3)}""")
+                        answers_probs = self.model([contexts_token_ids, queries_token_ids])
+                        answer_preds = answers_probs.argmax(dim=1)
+                        loss_step = loss(answers_probs, answers_tokens_ids)
+                        epoch_total_loss += loss_step.item()
+                        epoch_total_f1 += overlap_f1_score(answer_preds, answers_tokens_ids).mean().item()
+                        data_iter_tqdm.set_description(f"""[{epoch+1}/{epochs} - Validation] Loss: {
+                            round(epoch_total_loss/(i+1), 3)}, Overlap F1: {
+                                round(epoch_total_f1/(i+1), 3)}""")
