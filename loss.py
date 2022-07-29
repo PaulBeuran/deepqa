c343dc4905958c8563207607b731dd29fcb6b55a
Paul Beuran <paul.beuran@outlook.fr>
2022-07-29 11:38:20 +0200

    CUDA bug fix try

--- loss.py
+++ loss.py
@@ -1,5 +1,8 @@
 import torch
 
+if torch.cuda.is_available():
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
 def bi_cross_entropy(output, target):
   output_1, output_2 = output[:, :, 0], output[:, :, 1]
   target_1, target_2 = target[:, 0], target[:, 1]
